e0c6a0a2d09f5ba7b420f7c53438df0a71ac01d2
73359 <MichaelUhl1@gmx.de>
2023-07-27 15:38:33 +0100

    addDog test running

--- java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
@@ -1,16 +1,26 @@
 package com.db.grad.javaapi.service;
 
+import com.db.grad.javaapi.model.Dog;
+import com.db.grad.javaapi.repository.DogsRepository;
+import com.db.grad.javaapi.repository.DogsRepositoryStub;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DogsHandlerTest {
 
+    private DogsRepository itsDogRepo = new DogsRepositoryStub();
     @BeforeEach
     public void makeSureRepoIsEmpty() {
-        itsDogsRepo.deleteAll();
+        itsDogRepo.deleteAll();
     }
     @Test
     public void add_a_dog_return_number_of_dogs_in_repo_is_one() {
-        DogHandler cut = new DogHandler();
+        //arrange
+        DogHandler cut = new DogHandler( itsDogRepo );
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog(theDog);
+
+        int expectedResult = 1;
     }
 }
