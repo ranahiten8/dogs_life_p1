64aa63732008e707738a87a4ede09adbf997908f
73359 <MichaelUhl1@gmx.de>
2023-07-27 15:25:58 +0100

    first test written

---
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
@@ -0,0 +1,13 @@
+package com.db.grad.javaapi.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DogsHandlerTest {
+
+
+    @Test
+    public void add_a_dog_return_number_of_dogs_in_repo_is_one() {
+        DogHandler cut = new DogHandler();
+    }
+}
