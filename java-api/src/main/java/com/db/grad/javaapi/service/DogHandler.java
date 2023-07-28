2ccedcb928b503c04d767a213b24d4910a3fe53e
73359 <MichaelUhl1@gmx.de>
2023-07-28 10:01:43 +0100

    updated DogHandel and DogHandelTest To Finish

--- java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
@@ -3,6 +3,8 @@ package com.db.grad.javaapi.service;
 import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.repository.DogsRepository;
 
+import java.util.List;
+
 public class DogHandler {
     private DogsRepository itsDogsRepo;
     public DogHandler(DogsRepository repo) {
@@ -17,12 +19,39 @@ public class DogHandler {
         return itsDogsRepo.count();
     }
 
-    public Dog getDogById(long dogId){
-        return itsDogsRepo.findById(dogId);
+    public boolean removeDog(long uniqueId)
+    {
+        boolean result = false;
+
+        Dog theDog = itsDogsRepo.findById(uniqueId);
+        if(theDog != null)
+        {
+            result = itsDogsRepo.delete(theDog);
+        }
+
+        return  result;
     }
 
-    public long updateDogDetails(Dog theDog, long id) {
-        theDog.setId(id);
-        return theDog.getId();
+    public Dog getDogById(long uniqueId)
+    {
+        return itsDogsRepo.findById(uniqueId);
+    }
+
+    public Dog getDogByName(String dogsName )
+    {
+        Dog dogToFind = new Dog();
+        dogToFind.setName(dogsName);
+        List<Dog> dogs = itsDogsRepo.findByName(dogToFind);
+        Dog result = null;
+
+        if( dogs.size() == 1)
+            result = dogs.get(0);
+
+        return result;
+    }
+
+    public long updateDogDetails(Dog dogToUpdate)
+    {
+        return itsDogsRepo.save( dogToUpdate );
     }
 }
