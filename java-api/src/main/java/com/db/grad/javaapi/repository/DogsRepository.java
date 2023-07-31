447dff65336d8ec38395069646dd63b5d18f64cb
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2023-07-31 11:53:43 +0100

    removed Query annotators

--- java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepository.java
@@ -11,17 +11,17 @@ import java.util.List;
 @Repository
 public interface DogsRepository extends JpaRepository<Dog,Long> {
     long save( Dog theDog );
-    @Query(nativeQuery = true, value = "select dog_id from dogs where dog_id = :id")
+    //@Query(nativeQuery = true, value = "select dog_id from dogs where dog_id = :id")
     Dog  findById( long id );
     @Query(nativeQuery = true, value = "select * from dogs where name = :name")
     List<Dog> findByName(Dog name);
 
-    boolean delete( Dog theDog );
+    boolean delete(Dog theDog );
 
-    @Query(nativeQuery = true, value = "select count(dog_id) from dogs")
+    //@Query(nativeQuery = true, value = "select count(dog_id) from dogs")
     long count();
 
-    @Query(nativeQuery = true, value = "select from dogs where dog_id = :id")
+    //@Query(nativeQuery = true, value = "select from dogs where dog_id = :id")
     boolean existsById( long id );
 
     // Additional admin functionality
