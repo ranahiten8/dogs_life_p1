7f808b75edcd102ca5133b110d91d21edd62e765
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2023-07-31 10:32:33 +0100

    SQL Queries

--- java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepository.java
@@ -2,15 +2,22 @@ package com.db.grad.javaapi.repository;
 
 import com.db.grad.javaapi.model.Dog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+
 @Repository
 public interface DogsRepository extends JpaRepository<Dog,Long> {
     long save( Dog theDog );
+    @Query(nativeQuery = true, value = "select dog_id from dogs where dog_id = :id")
     Dog  findById( long id );
-    List<Dog> findByName(Dog theDog );
+    @Query(nativeQuery = true, value = "select * from dogs where name = :name")
+    List<Dog> findByName(Dog name);
     boolean delete( Dog theDog );
+
+    @Query(nativeQuery = true, value = "select count(dog_id) from dogs")
     long count();
     boolean existsById( long id );
 
