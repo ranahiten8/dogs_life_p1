4a3a26ed5d3ff766bdf507b3799912f9dbf6ef45
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2023-07-31 10:07:57 +0100

    all dogs

--- java-api/src/main/java/com/db/grad/javaapi/controller/DogController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/DogController.java
@@ -1,14 +1,20 @@
 package com.db.grad.javaapi.controller;
 
+import com.db.grad.javaapi.model.Dog;
+import com.db.grad.javaapi.service.DogService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class DogController {
-
+  private DogService itsDogService;
   @GetMapping("/")
   public String getWelcome() {
     return "Dogs API is up and running!";
   }
 
+  @GetMapping("/alldogs")
+  public List<Dog> getAllDogs(){return itsDogService.getAllDogs();}
 }
