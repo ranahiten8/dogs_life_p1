b78793abaa6a09b37c6fe1c6616c6e3261f29af6
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2023-08-01 12:23:56 +0100

    saving work

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import batman from '../Images/batman.jpg';
 
-const HeroDetail = () => {
-  return (
-    <div>
-      <img src={batman} alt="Batman" height="250px"/>
-      <p id="blue-alias">Alias: Batman</p>
-      <p className="green-class">Name: Bruce Wayne</p>
-      <button className="green-class">Like</button>   
-    </div>
-  )
+import images from '../index'
+
+const HeroDetail = (props) => {
+    console.log(props)
+    return (
+      <div>
+        <img src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>
+        <p id="blue-alias">Alias: {props.info.alias}</p>
+        <p className="green-class">Name: {props.info.name}</p>
+        <button className="green-class">Like</button>   
+      </div>
+    )
 }
 
 export default HeroDetail
--- ReactPractice/superheroes-ui/src/index.js
+++ ReactPractice/superheroes-ui/src/index.js
@@ -3,7 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Batman from "./Images/1.jpeg";
+import Superman from "./Images/2.jpeg";
+import GreenLantern from "./Images/4.jpeg";
+import TheHuntress from "./Images/5.jpeg";
+import TheFlash from "./Images/3.jpeg";
+import HarleyQuinn from "./Images/6.jpeg";
+import Deadshot from "./Images/7.jpeg";
+import CountVertigo from "./Images/8.jpeg";
+import Robin from "./Images/9.jpeg";
+import Nightwing from "./Images/10.jpeg";
 
+const images = {
+    Batman,
+    Superman,
+    GreenLantern,
+    TheHuntress,
+    TheFlash,
+    HarleyQuinn,
+    Deadshot,
+    CountVertigo,
+    Robin,
+    Nightwing
+}
+export default images;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
